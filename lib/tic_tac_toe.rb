470f880bc28d8759e189ea681e688f20f2eeb81f
Jacob Feldman <jacobgfeld@gmail.com>
2018-10-08 19:36:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -104,6 +104,7 @@ def winner(board)
 end
 
 def play(board)
+<<<<<<< HEAD
   until(over?(board))
     turn(board)
   end
@@ -112,6 +113,18 @@ def play(board)
   end
   if(draw?(board))
     puts "Cat's Game!"
+=======
+  for i in (0...9)
+    turn(board)
+    if(winner(board) != nil)
+      puts "test"
+      break
+    end
+    if(draw?(board))
+      puts "draw"
+      break
+    end
+>>>>>>> 4498f7c461f78e0b61df1f915dc17be818cf708c
   end
 end
 
